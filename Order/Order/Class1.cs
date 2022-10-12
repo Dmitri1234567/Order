d9381ac4c04d0fe7b3d5305c90523b2fe3ddb99d
Dima <dmitrijskurcenko3@gmail.com>
2022-10-12 11:43:50 +0300

    MyProject
    MyProject

---
+++ Order/Order/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Order
+{
+    internal class Class1
+    {
+        private String cookingName;
+        private String compound;
+        private String cookingLastName;
+        public String oreder;
+        public int i = 0;
+       
+
+        private int countCompound;
+
+        public List<String> Menu = new List<string>();
+        public List<String> Compound1 = new List<string>();
+       
+      
+        public void setCookingLastName(String cookingLastName)
+        {
+            this.cookingLastName = cookingLastName;
+        }
+        public String getCookingLastName()
+        {
+            return cookingLastName;
+        }
+      public void setCountCompound(int countCompound)
+        {
+            this.countCompound = countCompound;
+        }
+        public int getCountCompound()
+        {
+            return countCompound;
+        }
+
+        public void setCookingName(String cookingName)
+        {
+            this.cookingName = cookingName;
+        }
+        public String getCookingName()
+        {
+            return cookingName;
+        }
+        public void setCompound(String compound)
+        {
+            this.compound = compound;
+        }
+        public String getCompound()
+        {
+            return compound;
+        }
+    public void show()
+        {
+            foreach(string menu in Menu)
+            {
+                Console.WriteLine(menu);
+            }
+            foreach(string compound1 in Compound1)
+            {
+                Console.WriteLine(compound1);
+            }
+           
+           
+        }
+       
+
+        public void Order()
+        {
+           
+            Console.WriteLine("Enter your order please");
+            string order = Console.ReadLine();
+            Console.WriteLine(order);
+        
+
+
+
+
+        }
+
+    }
+}
---
+++ Order/Order/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Order
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            class1.setCookingName("John");
+            Console.WriteLine(class1.getCookingName());
+            class1.setCompound("Apple");
+            Console.WriteLine(class1.getCompound());
+            class1.Menu.Add("Pizza");
+           
+
+
+            class1.Menu.Add("Meat");
+            class1.Menu.Add("Apple Pie");
+
+
+            class1.Menu.Add("Ice cream");
+          
+
+
+            class1.Compound1.Add("sausages");
+          
+
+
+            class1.Compound1.Add("tomatoes");
+          
+
+     
+
+            class1.Compound1.Add("Shrimps");
+         
+
+            class1.show();
+            class1.Order();
+
+
+           
+           
+        }
+    }
+}
